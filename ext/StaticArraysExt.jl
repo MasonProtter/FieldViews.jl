3adf8a1143554880420455eb630d723c5c25f854
Mason Protter <mason.protter@icloud.com>
2025-11-12 20:10:51 +0100

    fix the StaticArraysExt

--- ext/StaticArraysExt.jl
+++ ext/StaticArraysExt.jl
@@ -3,7 +3,7 @@ module StaticArraysExt
 using FieldViews, StaticArrays
 
 function FieldViews.fieldmap(::Type{SA}) where {SA <: SVector}
-    if length(SA) < 4
+    if length(SA) <= 4
         names = (:x, :y, :z, :w)
         ntuple(Val(length(SA))) do i
             :data => Renamed(i, names[i])
